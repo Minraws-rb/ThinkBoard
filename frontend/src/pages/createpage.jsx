d4ed74349c59df0ea0a4092fcc282ac6f29fb720
Minraws-rb <pas081bct047@wrc.edu.np>
2026-02-23 23:13:12 +0545

    CreatePage

--- frontend/src/pages/createpage.jsx
+++ frontend/src/pages/createpage.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router";
+import { ArrowLeftIcon, Cone } from "lucide-react";
 
-const CreatePage = () => {
-  return  <div> CreatePage </div>;
-  
-}
+const CreatePage = ()=> {
+  const [title, setTitle] = useState("");
+  const [content,setContent] = useState("")
+  const [loading,setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    console.log(title)
+    console.log(content)
+  }
+
+  return <div className="min-h-screen bg-base-200">
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-2xl mx-auto">
+        <Link to={"/"} className="btn btn-ghost mb-6">
+        <ArrowLeftIcon className="size-5"/>
+        Back to Notes
+        </Link>
+        <div className="card bg-base-100">
+          <div className="card-body">
+            <h2 className="card-title text-2xl mb-4">Create New Notes</h2>
+            <form action="" onSubmit={handleSubmit}>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input type="text" 
+                placeholder="Note Title"
+                className="input input-bordered"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+                <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <input type="text" 
+                placeholder="Write your note here..."
+                className="input input-bordered h-32"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                />
+              </div>
+
+              <div className="card-actions justify-end">
+                <button type="submit" className="btn btn-primary" disabled={loading}>
+                  {loading ? "Creating..." : "Create Note"}
+                </button>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+};
 
 export default CreatePage;
